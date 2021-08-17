66e0fd4f7438876f0a4989ec2d52147b1f73ece3
Mike Tintes <michael.tintes@hotmail.com>
2021-08-17 15:53:04 -0500

    Update slack_send.sh

--- .github/actions/slack-publish/slack_send.sh
+++ .github/actions/slack-publish/slack_send.sh
@@ -1,1 +1,1 @@
-curl -X POST "https://slack.com/api/chat.postMessage" -H "Authorization: Bearer $SLACK_TOKEN" -H "Content-Type: application/json" -d $1
+curl -X POST "https://slack.com/api/chat.postMessage" -H "Authorization: Bearer $1" -H "Content-Type: application/json" -d $2
